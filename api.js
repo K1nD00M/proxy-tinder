97bd99f018ae6e021590abf191cf519d7c8ba1f6
Kirill <VolandB@gmail.com>
2024-05-14 10:05:18 +0300

    alpha version

---
+++ api.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const proxy = require('express-http-proxy');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+
+const PORT = 3001;
+const HOST = 'localhost';
+
+app.use(cors());
+app.use(morgan('dev'));
+
+// Функция для динамического определения целевого URL на основе поддомена
+function getTargetUrl(req) {
+  const host = req.headers.host;
+  const subdomainMatch = host.match(/^(.*?)\.localhost:3001$/);
+  if (subdomainMatch) {
+    const subdomain = subdomainMatch[1];
+    return `https://${subdomain}.gotinder.com`;
+  }
+  return null;
+}
+
+// Настройка прокси с динамическим целевым URL
+app.use('/', (req, res, next) => {
+  const targetUrl = getTargetUrl(req);
+  console.log(targetUrl)
+  if (targetUrl) {
+    proxy(targetUrl, {
+      proxyReqPathResolver: () => req.url
+    })(req, res, next);
+  } else {
+    res.status(404).send('Not Found');
+  }
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Starting Proxy at ${HOST}:${PORT}`);
+});
